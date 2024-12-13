f2b023c803df939cde4e2fa43a4e19af7c08071a
iharuya <i4haruya@gmail.com>
2024-12-14 03:18:04 +0900

    update scheduler with elaborated test cases

--- scheduler/mod.ts
+++ scheduler/mod.ts
@@ -7,35 +7,11 @@
  * import { Scheduler } from "jsr:@iharuya/scheduler";
  *
  * const scheduler = new Scheduler({ maxConcurrent: 2, minTime: 1000 });
- *
- * const task1 = async () => {
- *   console.log("Task 1 started");
- *   await new Promise(resolve => setTimeout(resolve, 2000));
- *   console.log("Task 1 finished");
- *   return 1;
- * };
- *
- * const task2 = async () => {
- *   console.log("Task 2 started");
- *   await new Promise(resolve => setTimeout(resolve, 3000));
- *   console.log("Task 2 finished");
- *   return 2;
- * };
- *
- * const task3 = async () => {
- *   console.log("Task 3 started");
- *   await new Promise(resolve => setTimeout(resolve, 1000));
- *   console.log("Task 3 finished");
- *   return 3;
- * };
- *
- * const result1 = scheduler.schedule(task1);
- * const result2 = scheduler.schedule(task2);
- * const result3 = scheduler.schedule(task3);
- *
- * Promise.all([result1, result2, result3]).then(results => {
- *   console.log("All tasks finished:", results);
- * });
+ * const tasks = [];
+ * for (let i = 0; i < 10; i++) {
+ *   tasks.push(scheduler.schedule(() => fetch(`https://example.com/api/${i}`)));
+ * }
+ * const results = await Promise.all(tasks);
  * ```
  */
 
@@ -53,46 +29,41 @@ export class Scheduler {
   #minTime: number;
   // deno-lint-ignore no-explicit-any
   #queue: QueueItem<any>[] = [];
-  #running: number = 0;
-  #lastRequestTime: number = 0;
+  #running = 0;
+  #lastRequestTime = 0;
 
   /**
    * Creates a new Scheduler instance.
    * @param options - Configuration options for the scheduler.
    * @param options.maxConcurrent - The maximum number of tasks that can run concurrently.
-   * @param options.minTime - The minimum time (in milliseconds) between the start of two consecutive tasks.
-   * @example
-   * ```ts
-   * const scheduler = new Scheduler({ maxConcurrent: 2, minTime: 1000 });
-   * ```
+   * @param options.minTime - The minimum time (in milliseconds) between the *finishing times* of consecutive tasks.
    */
   constructor(options: { maxConcurrent: number; minTime: number }) {
     this.#maxConcurrent = options.maxConcurrent;
     this.#minTime = options.minTime;
+    this.#lastRequestTime = 0;
   }
 
   async #run() {
     while (this.#queue.length > 0 && this.#running < this.#maxConcurrent) {
-      const waitTime = this.#minTime - (Date.now() - this.#lastRequestTime);
-      if (waitTime > 0) {
-        await new Promise((resolve) => setTimeout(resolve, waitTime));
-      }
-
-      const item = this.#queue.shift()!;
       this.#running++;
-      this.#lastRequestTime = Date.now();
-
-      try {
-        const result = await item.task();
-        item.resolve(result);
-      } catch (error) {
-        item.reject(error);
-      } finally {
+      const item = this.#queue.shift()!;
+      const elapsed = Date.now() - this.#lastRequestTime;
+      const waitTime = this.#minTime - elapsed;
+      if (waitTime > 0) {
+        await new Promise((r) => setTimeout(r, waitTime));
+      }
+      const promise = item.task();
+      promise.then(
+        (res) => item.resolve(res),
+        (err) => item.reject(err),
+      ).finally(() => {
+        this.#lastRequestTime = Date.now();
         this.#running--;
         if (this.#queue.length > 0) {
           queueMicrotask(() => this.#run());
         }
-      }
+      });
     }
   }
 
@@ -100,19 +71,11 @@ export class Scheduler {
    * Schedules a task to be executed.
    * @param task - An asynchronous function to be scheduled.
    * @returns A promise that resolves with the result of the task or rejects if the task throws an error.
-   * @template Res - The type of the task's result.
-   * @example
-   * ```ts
-   * const result = scheduler.schedule(async () => {
-   *   // Perform some asynchronous operation
-   *   return "result";
-   * });
-   * ```
    */
   schedule<Res>(task: () => Promise<Res>): Promise<Res> {
     return new Promise<Res>((resolve, reject) => {
       this.#queue.push({ task, resolve, reject });
-      this.#run();
+      queueMicrotask(() => this.#run());
     });
   }
 }
--- scheduler/test.ts
+++ scheduler/test.ts
@@ -1,69 +1,144 @@
 import {
+  assertAlmostEquals,
   assertEquals,
-  assertGreaterOrEqual,
-  assertLess,
   assertRejects,
 } from "jsr:@std/assert";
 import { sleep } from "jsr:@iharuya/time";
 import { Scheduler } from "./mod.ts";
 
-const mockResponse = new Response("Mock response body", { status: 200 });
-const mockFetch = async (_: RequestInfo, __?: RequestInit) => {
-  await sleep(100);
-  return mockResponse.clone();
-};
-const mockErrorFetch = async (_: RequestInfo, __?: RequestInit) => {
-  await sleep(100);
-  throw new Error("Mock fetch error");
-};
-
 Deno.test("Scheduler limits concurrent requests", async () => {
   const scheduler = new Scheduler({ maxConcurrent: 10, minTime: 0 });
-  const promises = [];
+  const tasks = [];
   for (let i = 0; i < 25; i++) {
-    promises.push(
-      scheduler.schedule(() => mockFetch("https://example.com")).then((res) =>
-        res.text()
-      ),
-    );
+    tasks.push(scheduler.schedule(() => sleep(100)));
   }
-
   const start = performance.now();
-  await Promise.all(promises);
+  await Promise.all(tasks);
   const end = performance.now();
   const elapsed = end - start;
-  assertGreaterOrEqual(elapsed, 300, "Elapsed time should be at least 300ms");
-  assertLess(elapsed, 400, "Elapsed time should be at most 400ms");
+  assertAlmostEquals(elapsed, 300, 20);
   // FIXME: もっと直接的にテストする方法を考えるべき
   // 実装側に手を加えて「タスク実行開始」イベントを発火させないといけない
 });
 
-Deno.test("Scheduler respects minTime", async () => {
-  const scheduler = new Scheduler({ maxConcurrent: 1, minTime: 100 });
+Deno.test("Scheduler limits concurrent requests even if tasks take arbitrary time", async () => {
+  const scheduler = new Scheduler({ maxConcurrent: 2, minTime: 0 });
+  const tasks = [];
+  tasks.push(scheduler.schedule(() => sleep(100)));
+  tasks.push(scheduler.schedule(() => sleep(50)));
+  tasks.push(scheduler.schedule(() => sleep(100)));
+  tasks.push(scheduler.schedule(() => sleep(50)));
+  tasks.push(scheduler.schedule(() => sleep(50)));
+  //# timeline ("|" = 50ms)
+  //1 ||
+  //2 |
+  //3  ||
+  //4   |
+  //5    |
   const start = performance.now();
-  await scheduler.schedule(() => mockFetch("https://example.com")).then((res) =>
-    res.text()
-  );
-  await scheduler.schedule(() => mockFetch("https://example.com")).then((res) =>
-    res.text()
-  );
+  await Promise.all(tasks);
   const end = performance.now();
   const elapsed = end - start;
-
-  assertGreaterOrEqual(elapsed, 200, "Elapsed time should be at least 200ms");
-  assertLess(elapsed, 300, "Elapsed time should be at most 300ms");
+  assertAlmostEquals(elapsed, 200, 20);
 });
 
-Deno.test("Scheduler response back to the caller", async () => {
-  const scheduler = new Scheduler({ maxConcurrent: 1, minTime: 0 });
-  const response = await scheduler.schedule(() =>
-    mockFetch("https://example.com")
-  ).then((res) => res.text());
-  assertEquals(response, "Mock response body");
+Deno.test("Scheduler limits concurrent requests even if tasks take arbitrary time #2", async () => {
+  const scheduler = new Scheduler({ maxConcurrent: 3, minTime: 0 });
+  const tasks = [];
+  tasks.push(scheduler.schedule(() => sleep(100)));
+  tasks.push(scheduler.schedule(() => sleep(50)));
+  tasks.push(scheduler.schedule(() => sleep(200)));
+  tasks.push(scheduler.schedule(() => sleep(50)));
+  tasks.push(scheduler.schedule(() => sleep(250)));
+  tasks.push(scheduler.schedule(() => sleep(50)));
+  tasks.push(scheduler.schedule(() => sleep(150)));
+  //# timeline ("|" = 50ms)
+  //1 ||
+  //2 |
+  //3 ||||
+  //4  |
+  //5   |||||
+  //6   |
+  //7    |||
+  const start = performance.now();
+  await Promise.all(tasks);
+  const end = performance.now();
+  const elapsed = end - start;
+  assertAlmostEquals(elapsed, 350, 20);
 });
 
-Deno.test("Scheduler throws error thrown in the callback", async () => {
+Deno.test("Scheduler respects minTime", async () => {
+  const scheduler = new Scheduler({ maxConcurrent: 1, minTime: 200 });
+  const tasks = [];
+  for (let i = 0; i < 3; i++) {
+    tasks.push(scheduler.schedule(() => sleep(100)));
+  }
+  const start = performance.now();
+  await Promise.all(tasks);
+  const end = performance.now();
+  const elapsed = end - start;
+  assertAlmostEquals(elapsed, 700, 20);
+});
+
+Deno.test("Scheduler respects minTime even if tasks take arbitrary time", async () => {
+  const scheduler = new Scheduler({ maxConcurrent: 1, minTime: 200 });
+  const tasks = [];
+  tasks.push(scheduler.schedule(() => sleep(50)));
+  tasks.push(scheduler.schedule(() => sleep(150)));
+  tasks.push(scheduler.schedule(() => sleep(100)));
+  const start = performance.now();
+  await Promise.all(tasks);
+  const end = performance.now();
+  const elapsed = end - start;
+  assertAlmostEquals(elapsed, 700, 20);
+});
+
+Deno.test("Scheduler limits concurrent requests and respects minTime", async () => {
+  const scheduler = new Scheduler({ maxConcurrent: 3, minTime: 200 });
+  const tasks = [];
+  for (let i = 0; i < 5; i++) {
+    tasks.push(scheduler.schedule(() => sleep(100)));
+  }
+  const start = performance.now();
+  await Promise.all(tasks);
+  const end = performance.now();
+  const elapsed = end - start;
+  assertAlmostEquals(elapsed, 400, 20);
+})
+
+Deno.test("Scheduler limits concurrent requests and respects minTime even if tasks take arbitrary time", async () => {
+  const scheduler = new Scheduler({ maxConcurrent: 3, minTime: 100 });
+  const tasks = [];
+  tasks.push(scheduler.schedule(() => sleep(100)));
+  tasks.push(scheduler.schedule(() => sleep(50)));
+  tasks.push(scheduler.schedule(() => sleep(200)));
+  tasks.push(scheduler.schedule(() => sleep(50)));
+  tasks.push(scheduler.schedule(() => sleep(250)));
+  tasks.push(scheduler.schedule(() => sleep(50)));
+  tasks.push(scheduler.schedule(() => sleep(150)));
+  //# timeline ("|" = 50ms)
+  //1 ||**
+  //2 |**
+  //3 ||||**
+  //4    |**
+  //5     |||||
+  //6       |**
+  //7       |||
+  const start = performance.now();
+  await Promise.all(tasks);
+  const end = performance.now();
+  const elapsed = end - start;
+  assertAlmostEquals(elapsed, 450, 20);
+})
+
+Deno.test("Scheduler task result goes back to the caller", async () => {
   const scheduler = new Scheduler({ maxConcurrent: 1, minTime: 0 });
-  const task = scheduler.schedule(() => mockErrorFetch("https://example.com"));
+  const result = await scheduler.schedule(() => Promise.resolve("success"));
+  assertEquals(result, "success");
+});
+
+Deno.test("Scheduler task throws error thrown in the callback", async () => {
+  const scheduler = new Scheduler({ maxConcurrent: 1, minTime: 0 });
+  const task = scheduler.schedule(() => Promise.reject(new Error("failure")));
   await assertRejects(() => task);
 });
